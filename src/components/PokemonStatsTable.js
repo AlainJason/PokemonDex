88bfdd413df2a7477e671a9a2b130c1c94c1040c
Jason <jasonforwork030030@gmail.com>
2023-03-21 18:14:59 +0800

    add min & max base stat value

--- src/components/PokemonStatsTable.js
+++ src/components/PokemonStatsTable.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import { TableContainer2, DataTable, Barchart} from '../components_styled/Table.Styled'
+
+/*
+  hp max = basehp * 2 + 204
+     min = basehp * 2 + 110
+  else
+     max = (base * 2 + 99) * 1.1
+     min = (base * 2 + 5) * 0.9
+*/
+
+const calMinMax = (name , stats) => {
+  if(name === 'hp'){
+    const max = stats * 2 + 204;
+    const min = stats * 2 + 110;
+    return [min, max];  
+  }
+  else{
+    const max = (stats * 2 + 99) * 1.1;
+    const min = (stats * 2 + 5) * 0.9;
+    return [min, max];  
+  }
+}
+
 const PokemonStatsTable = (props) => {
   const  P  = props.pokemon
   return (
@@ -13,6 +35,8 @@ const PokemonStatsTable = (props) => {
           <Barchart state={P.states.hp}>
             <div></div>
           </Barchart>
+          <td>{Math.floor(calMinMax("hp", P.states.hp)[0])}</td>
+          <td>{Math.floor(calMinMax("hp", P.states.hp)[1])}</td>
         </tr>
         <tr>
           <th>Attack</th>
@@ -20,6 +44,8 @@ const PokemonStatsTable = (props) => {
           <Barchart state={P.states.attack}>
             <div></div>
           </Barchart>
+          <td>{Math.floor(calMinMax("Attack", P.states.attack)[0])}</td>
+          <td>{Math.floor(calMinMax("Attack", P.states.attack)[1])}</td>
         </tr>
         <tr>
           <th>Defense</th>
@@ -27,6 +53,8 @@ const PokemonStatsTable = (props) => {
           <Barchart state={P.states.defense}>
             <div></div>
           </Barchart>
+          <td>{Math.floor(calMinMax("Defense", P.states.defense)[0])}</td>
+          <td>{Math.floor(calMinMax("Defense", P.states.defense)[1])}</td>
         </tr>
         <tr>
           <th>SP.attack</th>
@@ -34,6 +62,8 @@ const PokemonStatsTable = (props) => {
           <Barchart state={P.states.special_A}>
             <div></div>
           </Barchart>
+          <td>{Math.floor(calMinMax("special_A", P.states.special_A)[0])}</td>
+          <td>{Math.floor(calMinMax("special_A", P.states.special_A)[1])}</td>
         </tr>
         <tr>
           <th>SP.Defense</th>
@@ -41,6 +71,8 @@ const PokemonStatsTable = (props) => {
           <Barchart state={P.states.special_D}>
             <div></div>
           </Barchart>
+          <td>{Math.floor(calMinMax("special_D", P.states.special_D)[0])}</td>
+          <td>{Math.floor(calMinMax("special_D", P.states.special_D)[1])}</td>
         </tr>
         <tr>
           <th>Speed</th>
@@ -48,6 +80,8 @@ const PokemonStatsTable = (props) => {
           <Barchart state={P.states.speed}>
             <div></div>
           </Barchart>
+          <td>{Math.floor(calMinMax("speed", P.states.speed)[0])}</td>
+          <td>{Math.floor(calMinMax("speed", P.states.speed)[1])}</td>
         </tr>
         <tr>
           <th>Total</th>
@@ -56,7 +90,12 @@ const PokemonStatsTable = (props) => {
                P.states.defense + 
                P.states.special_A +
                P.states.special_D +
-               P.states.speed}</td>
+               P.states.speed}
+          </td>
+          <Barchart>
+          </Barchart>
+          <td>Min</td>
+          <td>Max</td>
         </tr>
       </tbody>
       </DataTable>               
